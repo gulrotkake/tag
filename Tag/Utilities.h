95570dcb6417a8e024df69f741fe3cc72fd20df4
Michael Mortensen <michael@f3k.org>
2024-07-04 00:16:14 +0200

    Remove dead code

--- Tag/Utilities.h
+++ Tag/Utilities.h
@@ -2,9 +2,8 @@
 
 @interface Utilities : NSObject
 
-+ (NSImage*)rotateImage:(NSImage*)image degrees:(CGFloat)deg;
 + (NSString*)getUTCDate:(NSDate*)localDate;
 + (NSDate*)getStartOfDay:(NSDate*)date;
 + (NSDate*)getDateFromUTCTimestamp:(NSString*)date;
-+ (NSImage*)scaleImage:(NSImage*)image longSide:(CGFloat)longSide;
+
 @end
--- Tag/Utilities.m
+++ Tag/Utilities.m
@@ -25,42 +25,4 @@
     components.minute = 0;
     return [gregorian dateFromComponents:components];
 }
-
-+ (NSImage*)rotateImage:(NSImage*)image degrees:(CGFloat)deg {
-    NSSize existingSize;
-
-    NSBitmapImageRep* rep = [NSBitmapImageRep imageRepWithData:[image TIFFRepresentation]];
-
-    existingSize.width = [rep pixelsWide];
-    existingSize.height = [rep pixelsHigh];
-
-    NSSize newSize = NSMakeSize(existingSize.height, existingSize.width);
-    NSImage* rotatedImage = [[NSImage alloc] initWithSize:newSize];
-
-    [rotatedImage lockFocus];
-
-    NSAffineTransform* rotate = [NSAffineTransform transform];
-    NSPoint centerPoint = NSMakePoint(newSize.width / 2, newSize.height / 2);
-
-    [rotate translateXBy:centerPoint.x yBy:centerPoint.y];
-    [rotate rotateByDegrees:deg];
-    [rotate translateXBy:-centerPoint.y yBy:-centerPoint.x];
-    [rotate concat];
-
-    NSRect r1 = NSMakeRect(0, 0, newSize.height, newSize.width);
-    [rep drawInRect:r1];
-
-    [rotatedImage unlockFocus];
-    [rotatedImage autorelease];
-    return rotatedImage;
-}
-
-+ (NSImage*)scaleImage:(NSImage*)image longSide:(CGFloat)longSide {
-    NSSize size = [image size];
-    CGFloat ratio = size.width > size.height ? longSide / size.width : longSide / size.height;
-    size.width *= ratio;
-    size.height *= ratio;
-    [image setSize:size];
-    return image;
-}
 @end
